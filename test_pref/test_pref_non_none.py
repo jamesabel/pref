87b1410575ecc04fe2c40a3bc7185de3f6df2ced
James Abel <j@abel.co>
2020-12-25 17:54:22 -0800

    add assert for non-None default

--- test_pref/test_pref_non_none.py
+++ test_pref/test_pref_non_none.py
@@ -23,16 +23,17 @@ def test_preferences():
     assert not preferences.my_other_bool
     assert preferences.my_str == my_default_string
 
-    preferences.my_bool = False
+    preferences.my_bool = False  # was True, now set to False
     preferences.my_str = my_new_string
     assert not preferences.my_bool
     assert not preferences.my_other_bool
     assert preferences.my_str == my_new_string
 
-    preferences.my_other_bool = False
+    preferences.my_other_bool = False  # same value as default - should not even write to DB
     assert not preferences.my_other_bool
 
     # now make sure what we wrote out is still in the DB
     preferences = PrefTst(__application_name__, __author__)
     assert not preferences.my_bool
+    assert not preferences.my_other_bool
     assert preferences.my_str == my_new_string
