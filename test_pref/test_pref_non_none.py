23a8dc0c67f2bf3df0f517c4a3c023c85b810f46
James Abel <j@abel.co>
2020-12-25 17:07:33 -0800

    add more test conditions

--- test_pref/test_pref_non_none.py
+++ test_pref/test_pref_non_none.py
@@ -10,7 +10,8 @@ my_new_string = "I am new"
 
 @attrs
 class PrefTst(Pref):
-    my_bool: bool = attrib(default=False)
+    my_bool: bool = attrib(default=True)
+    my_other_bool: bool = attrib(default=False)
     my_str: bool = attrib(default=my_default_string)
 
 
@@ -18,10 +19,15 @@ def test_preferences():
     assert not get_sqlite_path(__application_name__, __author__).exists()  # check that the testing framework starts without at DB file
     preferences = PrefTst(__application_name__, __author__)
 
-    assert not preferences.my_bool
+    assert preferences.my_bool
+    assert not preferences.my_other_bool
     assert preferences.my_str == my_default_string
 
-    preferences.my_bool = True
+    preferences.my_bool = False
     preferences.my_str = my_new_string
-    assert preferences.my_bool
+    assert not preferences.my_bool
+    assert not preferences.my_other_bool
     assert preferences.my_str == my_new_string
+
+    preferences.my_other_bool = False
+    assert not preferences.my_other_bool
